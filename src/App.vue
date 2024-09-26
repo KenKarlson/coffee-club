2575055043325970dba17db3c22fbe65b9854be6
Гусь Гитарист <kenkarlson@mail.ru>
2024-09-26 16:38:32 +0300

    Create first page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <div id="app">
-    <h1>Hello & welcome !!!</h1>
-  </div>
+    <div class="wrapper">
+        <hero-view />
+        <footer-component />
+    </div>
 </template>
 
 <script>
-
-
+import HeroView from '@/views/HeroView.vue';
+import FooterComponent from './components/FooterComponent.vue';
 export default {
+    components: { HeroView, FooterComponent },
+    //components: { FooterComponent}
 }
 </script>
 
-<style>
-</style>
+<style lang="scss"></style>
