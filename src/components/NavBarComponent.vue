f4cda6e4713f7110369dda0b6788de78c837e62c
Ken Karlson <javarecords@mail.ru>
2024-09-28 14:34:51 +0300

    Add new pages

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -2,18 +2,26 @@
   <header>
     <ul class="header d-flex justify-content-center justify-content-md-start flex-wrap">
       <li class="header__item">
-        <a href="./index.html">
+        <router-link to="/">
           <img src="@/assets/Logo.svg" alt="logo">
-        </a>
+        </router-link>
+        <!-- <a href="./index.html">
+          <img src="@/assets/Logo.svg" alt="logo">
+        </a> -->
       </li>
       <li class="header__item">
-        <a href="./coffeepage.html">Our coffee</a>
+        <router-link to="/our-coffee">Our coffe</router-link>
+        <!-- <a href="./coffeepage.html">Our coffee</a> -->
       </li>
       <li class="header__item">
-        <a href="./goodspage.html">For your pleasure</a>
+        <router-link to="/goodspage">For your pleasure</router-link>
       </li>
       <li class="header__item">
-        <a href="./contacts.html">Contact us</a>
+        <router-link to="/contacts">Contact us</router-link>
+      </li>
+      <li class="header__item">
+        <router-link to="/thanks">Thanks</router-link>
+        <!-- <a href="./coffeepage.html">Our coffee</a> -->
       </li>
     </ul>
   </header>
