66831fae9fc589dc8b4f8522558eba76b5126b84
Гусь Гитарист <javarecords@mail.ru>
2025-04-14 12:58:09 +0300

    fix router & import style

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HeroView from "../views/HeroView.vue";
+import HeroView from "@/views/HeroView.vue";
 import OurCoffeeView from "@/views/OurCoffeeView.vue";
 import ContactView from "@/views/ContactView.vue";
 import ForYouPleasure from "@/views/ForYouPleasure.vue";
@@ -9,13 +9,31 @@ import ThankYouView from "@/views/ThankYouView.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  { path: "/", component: HeroView },
-  { path: "/index.html", component: HeroView },
-  { path: "/our-coffee", component: OurCoffeeView },
-  { path: "/coffeepage.html", component: OurCoffeeView },
-  { path: "/contacts.html", component: ContactView },
-  { path: "/goodspage.html", component: ForYouPleasure },
-  { path: "/thanks.html", component: ThankYouView },
+  {
+    path: "/",
+    name: "hero",
+    component: HeroView,
+  },
+  {
+    path: "/our-coffee",
+    name: "our-coffee",
+    component: OurCoffeeView,
+  },
+  {
+    path: "/contacts",
+    name: "contacts",
+    component: ContactView,
+  },
+  {
+    path: "/goodspage",
+    name: "goodspage",
+    component: ForYouPleasure,
+  },
+  {
+    path: "/thanks",
+    name: "thanks",
+    component: ThankYouView,
+  },
 ];
 
 const router = new VueRouter({
